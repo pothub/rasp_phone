a4f347e1122ad4c48ab6c0f4a04677668608e559
Your Name <you@example.com>
2017-08-26 03:36:11 +0000

    recv_920 fix

--- recv_920.py
+++ recv_920.py
@@ -1,4 +1,5 @@
 import serial
+import os
 ser = serial.Serial('/dev/ttyUSB0',115200)
 ser.write("listen\n")
 print ser.read(5)
@@ -9,13 +10,16 @@ while 1:
         print "end"
         file_flag = 0
         f.close()
+        os.system("cp recv.bin copy.mp3")
+        os.system("mpg321 copy.mp3 &")
     if file_flag == 1:
-        print line
+        # print line
         f.write(line)
 
     if line.find("recvb=") >=0:
+        os.system("rm recv.bin")
         print "start"
         file_flag = 1
-        print line[8:]
+        # print line[8:]
         f = open('recv.bin','ab')
-        f.write(line[8:])
+	f.write(line[8:])
